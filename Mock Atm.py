d980de0dae22cbc2bc1ec3339ba74cfa92269bfb
DevBasito <58977146+DevBasito@users.noreply.github.com>
2021-04-12 02:46:09 +0100

    Add files via upload

---
+++ Mock Atm.py
@@ -0,0 +1,102 @@
+import random
+
+database = {8944617295: ['Basit', 'Ade', 'baxx@gmail.com', 'password101']}
+
+"""
+Added items to the dictionary because of the logIn Option without having to register first
+The register option works fine too
+"""
+
+def init():
+    isValidOption = False
+    print('='*50)
+    print('Welcome to Bank FTMB!!!')
+    print('='*50)
+
+    haveAccount = int(input('Do you have an Account with us 1-(YES) 2-(NO): \n'))
+    if (haveAccount == 1):
+        logIn()
+    elif (haveAccount == 2):
+        register()
+    else:
+        print('Invalid Option Selected')
+        init()
+
+
+def register():
+    print('*********Register*********')
+    firstName = input('What is your First Name \n')
+    lastName = input('What is your Last Name \n')
+    email = input('What is your Email Address \n')
+    pword = input('Create a secure Password for yourself \n')
+    accountNumber = generatingAcctNum()
+
+    database[accountNumber] = [firstName, lastName, email, pword]
+    print('='*50)
+    print(f'Your New Account Number is {accountNumber}')
+    print('='*50)
+    print('\n')
+    
+    logIn()
+
+
+def logIn():
+    print('*********Log In*********')
+
+    acctnumFromUser = int(input("Please Enter your Account Number \n"))
+    password = input('Enter your Password \n')
+
+    for accountNumber, userDetails in database.items():
+        if (acctnumFromUser == accountNumber):
+            if (userDetails[3] == password):
+                bankOperations(userDetails)
+            
+            else:
+                print('Invalid account or password')
+                logIn()
+
+
+def generatingAcctNum():
+    return random.randrange(1111111111, 9999999999)
+
+def bankOperations(user):
+    print('='*50)
+    print(f'Welcome {user[0]} {user[1]}')
+    print('='*50)
+    option1 = int(input('What do you want to do (1)-Deposit (2)-Withdrawal (3)-Complaint (4)-Exit (5)-LogOut \n'))
+
+    if (option1 == 1):
+        deposit()
+    elif (option1 == 2):
+        withdrawal()
+    elif (option1 == 3):
+        complaint()
+    elif (option1 == 4):
+        exit()
+    elif (option1 == 5):
+        logout()
+    else:
+        print('Invalid Option Selected')
+        bankOperations(user)
+
+
+def deposit():
+    deposit = input('How much would you like to deposit: \n')
+    print(f'Current Account Balance: NGN {deposit}')
+    print('Thank you for banking with us')
+
+def withdrawal():
+    withdrawal = input('How much would you like to withdraw: \n')
+    print(f'Take Your Cash: NGN {withdrawal}')
+    print('Thank you for banking with us')
+
+def complaint():
+    complaint = input('What issue will you like to report? \n')
+    print('Thank you for contacting us. Your Issue will be resolved soon')
+
+def logout():
+    logIn()
+
+init()    
+
+
